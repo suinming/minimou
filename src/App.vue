74ef4ddd7f4195989c2ed34c6b77bf1d9fb3c5ff
suinming <0223314338aa@gmail.com>
2022-03-19 18:45:15 +0800

    navBar finished

---
+++ src/App.vue
@@ -0,0 +1,161 @@
+<template>
+  <header v-if="atTop" class="header">
+        <div class="navbarflexWrapper">
+            <ul class="navbarLeft">
+                <li ><a href="#">首頁</a></li>
+                <li ><a href="#">門市資訊</a></li>
+                <li ><a href="#">最新消息</a></li>
+                <li ><a href="#">餐點介紹</a></li>
+            </ul>
+            <img src="@/assets/img/minimouBlackLogo.png" alt="minimouLogo" />
+            <ul class="navbarRight">
+                <li ><a href="#">品牌介紹</a></li>
+                <li ><a href="#" >加入我們</a></li>
+                <li ><a href="#" >加盟專區</a></li>
+                <li ><a href="#" >線上購物</a></li>
+            </ul>
+        </div>
+    </header>
+    <header v-else class="headerOnscroll">
+      <img src="@/assets/img/minimouWhiteLogo.png" alt="minimouLogo" />
+        <ul class="navbarOnscroll">
+            <li ><a href="#">首頁</a></li>
+            <li ><a href="#">門市資訊</a></li>
+            <li ><a href="#">最新消息</a></li>
+            <li ><a href="#">餐點介紹</a></li>
+            <li ><a href="#">品牌介紹</a></li>
+            <li ><a href="#" >加入我們</a></li>
+            <li ><a href="#" >加盟專區</a></li>
+            <li ><a href="#" >線上購物</a></li>
+        </ul>
+    </header>
+  <router-view/>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      atTop: true
+    }
+  },
+  created () {
+    window.addEventListener('scroll', this.onScroll)
+  },
+  unmounted () {
+    window.removeEventListener('scroll', this.onScroll)
+  },
+  methods: {
+    onScroll () {
+      window.scrollY < 5 ? this.atTop = true : this.atTop = false
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  max-width: 1360px;
+  margin: 0 auto;
+}
+
+.container{
+  width: 100%;
+}
+
+/* section*/
+section{
+  font-size: 25px;
+}
+
+</style>
+
+<style>
+/* top navbar */
+header{
+  max-height: 160px;
+  font-size: 14px;
+  color:var(--black);
+  background: var(--black);
+  padding-left:76px;
+  padding-right: 76px;
+}
+
+.onScroll{
+  max-height: 64px;
+}
+
+.navbarflexWrapper{
+  display: flex;
+  justify-content: space-between;
+}
+
+.navbarLeft, .navbarRight{
+  display: flex;
+  max-width: 398px;
+  max-height: 27px;
+  width: 100%;
+  justify-content: space-between;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  margin-top: 109px;
+  margin-bottom: 24px;
+}
+
+.navbarLeft li a, .navbarRight li a{
+  color: white;
+  font-weight: 700;
+}
+
+.navbarLeft li a:hover, .navbarRight li a:hover, .navbarOnscroll li a:hover{
+  text-decoration: underline;
+  text-underline-offset: 4px;
+  text-decoration-thickness: 4px;
+}
+
+header img{
+  max-width: 112px;
+  max-height: 112px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 40px;
+}
+
+/* onscroll navbar*/
+.headerOnscroll{
+  display: flex;
+  justify-content: space-between;
+  position: sticky;
+  top:0;
+  max-height: 64px;
+  background: white;
+  padding-right: 76px;
+  padding-left: 76px;
+}
+
+.headerOnscroll img{
+  max-width: 147px;
+  max-height: 42px;
+  margin: 0;
+}
+
+.navbarOnscroll{
+  display: flex;
+  justify-content: space-between;
+  max-width: 588px;
+  width: 100%;
+  max-height: 27px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+}
+
+.navbarOnscroll li a{
+  color: var(--black);
+}
+
+.navbarOnscroll li a:hover{
+  text-decoration-color: var(--red);
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+ Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam debitis odio beatae commodi alias suscipit deleniti aliquam, iure quam pariatur repellendus amet sapiente corporis deserunt cum. Porro eveniet reiciendis magnam!
+ Harum alias sapiente ipsum autem ullam, illum adipisci unde aspernatur eius quibusdam suscipit sed fugit maiores, perspiciatis esse nisi minima. Nihil totam excepturi ea, quis natus sit at! Facere, impedit!
+ Velit soluta veniam eos, atque nulla necessitatibus cum labore at, quis eligendi est laborum, consequatur repellendus quas maiores ea architecto provident doloremque! Ducimus, itaque aut iusto aliquid molestias delectus enim?
+ Totam quam amet minus veniam perferendis ut magni animi. Odit, deleniti! Commodi facere cumque, doloremque perferendis eveniet explicabo delectus. Sequi numquam neque, veritatis facere tenetur itaque quisquam quasi aliquam laudantium?
+ Vero qui blanditiis neque eum dolorum earum consectetur ex sapiente aliquam accusamus. Possimus mollitia incidunt quis non perspiciatis commodi debitis alias animi expedita asperiores! Aliquid magni quas delectus aperiam et?
+ Vel obcaecati sed deserunt? Illum rerum suscipit omnis ad at nisi commodi repellat deleniti. Ab sapiente tenetur, nulla doloribus consectetur laborum laboriosam consequuntur, architecto quibusdam quam autem mollitia esse reiciendis.
+ Adipisci, illo, vitae qui omnis iure similique perferendis dolor id quibusdam reprehenderit eum assumenda possimus saepe inventore sint animi. Impedit eveniet facilis nulla repudiandae ad ipsum excepturi optio quia nesciunt.
+ Dignissimos quae pariatur voluptate dolor laudantium. Minima sequi harum at mollitia quisquam debitis recusandae, voluptate sunt officiis velit atque sed quis natus eos ut hic eaque odit non, ipsa praesentium.
+ Ex atque eum corrupti at laborum nesciunt qui impedit quaerat! Illum, quos dicta. Sint, velit reprehenderit eligendi fugiat eveniet in omnis aut nam accusamus magnam dolore, autem at explicabo consequatur!
+ Iusto est mollitia, minima natus obcaecati delectus minus illo ipsam, at quisquam repellat. Necessitatibus consequuntur odio impedit assumenda aperiam quo deserunt, distinctio nostrum minima quod, nam voluptates cumque? Maiores, et!
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni laborum adipisci atque libero, ut accusamus ea voluptatem nostrum aliquid, sapiente numquam ex ipsa debitis. Officiis praesentium doloremque at cumque possimus.
+  Quis ullam error ea recusandae voluptatem mollitia harum et repudiandae, doloremque accusamus similique praesentium adipisci alias repellendus! Dolore, quasi commodi. Enim voluptatum impedit commodi tempora, quod dolorem veritatis magnam hic.
+  Asperiores fugit quos libero laboriosam inventore minus ad minima saepe necessitatibus laudantium id aspernatur, recusandae, iure commodi. Corrupti rem provident aliquam totam. Voluptatibus sit, rerum laudantium cum veritatis libero praesentium!
+  Commodi voluptatum error veniam doloremque, accusamus iste quae eius consectetur ab dignissimos quibusdam reiciendis perferendis est sunt magnam impedit expedita in ut repellendus quidem eaque ullam vitae officia et? Sed.
+  Cum voluptates animi, iure dolor a voluptatem ex eveniet eius repellat harum perferendis facilis unde culpa, aliquam suscipit eligendi, non ratione tenetur nostrum debitis. Voluptatum eius dolore repudiandae nobis mollitia?
+  Ducimus inventore neque tempora repudiandae vitae aspernatur. Ea pariatur veniam dolorem labore possimus, iste excepturi quisquam doloribus dignissimos in minima qui ex praesentium quis aspernatur cumque et error laboriosam. Fugiat.
+  Provident, natus animi facere tempore consequatur dolorem delectus et ad placeat eius architecto illum perferendis cumque dolores atque, alias sequi quas laudantium fugiat numquam assumenda rem veniam necessitatibus minus. Ab.
+  Minima recusandae porro cum laudantium non unde libero facere tempora velit reiciendis. Expedita corporis quidem ipsum aliquam dicta quas, numquam recusandae magnam fugiat voluptatibus odio eum minus tempora impedit at.
+  Optio minima hic assumenda deserunt aspernatur ratione illo, ullam in porro eos qui vitae impedit unde iure quia veniam adipisci ut ea repudiandae odit distinctio autem. Magni ratione minima expedita?
+  Aut cum labore error asperiores sit quidem ea alias doloremque, temporibus repellendus minima optio molestias eligendi eum, deleniti nulla! Voluptas, deleniti consectetur? Nesciunt excepturi quod iusto. Laudantium debitis soluta deserunt!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  font-size: 20px;
+}
+</style>
